903b28998eccf00e4ab892e550f99d5ff9b03d41
Laura Deng <lauradeng@Lauras-MacBook-Pro.local>
2023-08-29 23:42:12 -0500

    device_config_controller_test.go without mocks

--- internal/controllers/device_config_controller_test.go
+++ internal/controllers/device_config_controller_test.go
@@ -11,26 +11,23 @@ import (
 	"github.com/DIMO-Network/vehicle-signal-decoding/internal/config"
 	"github.com/DIMO-Network/vehicle-signal-decoding/internal/test"
 	"github.com/gofiber/fiber/v2"
-	"github.com/golang/mock/gomock"
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/assert"
-	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
 const migrationsDirRelPath = "../../migrations"
 
 func TestGetPIDsByTemplate(t *testing.T) {
 
-	// Arrange: global db and route setup
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-
+	// Arrange: db and route setup
 	logger := zerolog.New(os.Stdout).With().
 		Timestamp().
 		Str("app", "vehicle-signal-decoding").
 		Logger()
 
 	ctx := context.Background()
+
+	//Spin up test database in a Docker container
 	pdb, container := test.StartContainerDatabase(ctx, t, migrationsDirRelPath)
 	defer func() {
 		if err := container.Terminate(ctx); err != nil {
@@ -38,29 +35,27 @@ func TestGetPIDsByTemplate(t *testing.T) {
 		}
 	}()
 
-	pidService := mock_services.NewMockPIDService(mockCtrl)
-
+	// Insert test data into database
 	templateName := "exampleTemplate"
-	c := NewDeviceConfigController(&config.Settings{Port: "3000"}, &logger, pidService, pdb.DBS)
+	samplePID := PIDConfig{
+		ID:              1,
+		Header:          "7E8",
+		Mode:            "01",
+		Pid:             "05",
+		Formula:         "A*5",
+		IntervalSeconds: 60,
+		Version:         "1.0",
+	}
+
+	_, err := pdb.DBS().Writer.ExecContext(ctx,
+		`INSERT INTO pid_config (id, header, mode, pid, formula, interval_seconds, version, template_name) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+		samplePID.ID, samplePID.Header, samplePID.Mode, samplePID.Pid, samplePID.Formula, samplePID.IntervalSeconds, samplePID.Version, templateName)
+
+	c := NewDeviceConfigController(&config.Settings{Port: "3000"}, &logger, pdb.DBS().Reader.DB)
 	app := fiber.New()
 	app.Get("/device-config/pid/:template_name", c.GetPIDsByTemplate)
 
 	t.Run("GET - PID by Template", func(t *testing.T) {
-		// Arrange: db, insert some PIDConfig data
-		samplePID := PIDConfig{
-			Name:            "EngineTemperature",
-			Header:          "7E8",
-			Mode:            "01",
-			PID:             "05",
-			Formula:         "A*5",
-			IntervalSeconds: 60,
-			TemplateName:    templateName,
-		}
-		err := samplePID.Insert(ctx, pdb.DBS().Writer, boil.Infer())
-		assert.NoError(t, err)
-
-		pidService.EXPECT().GetPIDsByTemplate(gomock.Any(), templateName).Times(1).
-			Return([]models.PIDConfig{samplePID}, nil)
 
 		// Act: make the request
 		request := test.BuildRequest("GET", "/device-config/pid/"+templateName, "")
@@ -76,9 +71,6 @@ func TestGetPIDsByTemplate(t *testing.T) {
 		err = json.Unmarshal(body, &pids)
 		assert.NoError(t, err)
 		assert.Equal(t, 1, len(pids))
-		assert.Equal(t, samplePID.Name, pids[0].Name)
-
-		// Teardown: cleanup after test
-		test.TruncateTables(pdb.DBS().Writer.DB, t)
+		assert.Equal(t, samplePID.ID, pids[0].ID)
 	})
 }
