44d6c2f3e29bc86121a3e0330d3a499702acca3c
Laura Deng <lauradeng@Lauras-MacBook-Pro.local>
2023-10-16 14:18:52 -0500

    modifed device settings tests and migration

--- internal/infrastructure/db/migrations/20231012142752_add_msg_col_position.sql
+++ internal/infrastructure/db/migrations/20231012142752_add_msg_col_position.sql
@@ -2,9 +2,7 @@
 -- +goose StatementBegin
 SELECT 'up SQL query';
 
-ALTER TABLE pid_configs ADD COLUMN message_column_position INT;
-UPDATE pid_configs SET message_column_position = 0 WHERE message_column_position IS NULL;
-ALTER TABLE pid_configs ALTER COLUMN message_column_position SET NOT NULL;
+ALTER TABLE pid_configs ADD COLUMN message_column_position INT DEFAULT 0;
 
 -- +goose StatementEnd
 
