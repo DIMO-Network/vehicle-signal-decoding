ae798e0194caecf3915deb17fe9e966e8268c422
James Reategui <JamesReate@users.noreply.github.com>
2023-11-27 21:10:03 -0500

    update seed

--- seed.sql
+++ seed.sql
@@ -14,81 +14,81 @@ INSERT INTO device_settings (template_name, battery_critical_level_voltage, safe
 
 
 INSERT INTO pid_configs (template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES ('default-ice-can11', E'\\x30', '31|8@0+ (1,0) [0|255] "count"', 0, 'CAN11_500', 'warmupsSinceDtcClear');
+VALUES ('default-ice-can11', E'\\x30', 'dbc: 31|8@0+ (1,0) [0|255] "count"', 0, 'CAN11_500', 'warmupsSinceDtcClear');
 INSERT INTO pid_configs (template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES ('default-ice-can11', E'\\x31', '31|16@0+ (1,0) [0|65535] "km" ', 0, 'CAN11_500', 'distanceSinceDtcClear');
+VALUES ('default-ice-can11', E'\\x31', 'dbc: 31|16@0+ (1,0) [0|65535] "km" ', 0, 'CAN11_500', 'distanceSinceDtcClear');
 INSERT INTO pid_configs (template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES ('default-ice-can11', E'\\x21', '31|16@0+ (1,0) [0|65535] "km"', 0, 'CAN11_500', 'distanceWMil');
+VALUES ('default-ice-can11', E'\\x21', 'dbc: 31|16@0+ (1,0) [0|65535] "km"', 0, 'CAN11_500', 'distanceWMil');
 INSERT INTO pid_configs (template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES ('default-ice-can11', E'\\x2c', '31|8@0+ (0.392156862745098,0) [0|100] "%"', 20, 'CAN11_500', 'commandedEgr');
+VALUES ('default-ice-can11', E'\\x2c', 'dbc: 31|8@0+ (0.392156862745098,0) [0|100] "%"', 20, 'CAN11_500', 'commandedEgr');
 INSERT INTO pid_configs (template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES ('default-ice-can11', E'\\x1f', '31|16@0+ (1,0) [0|65535] "seconds"', 30, 'CAN11_500', 'runTime');
+VALUES ('default-ice-can11', E'\\x1f', 'dbc: 31|16@0+ (1,0) [0|65535] "seconds"', 30, 'CAN11_500', 'runTime');
 INSERT INTO pid_configs (template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES ('default-ice-can11', E'\\x06', '31|8@0+ (0.78125,-100) [-100|99.21875] "%"', 10, 'CAN11_500', 'shortFuelTrim');
+VALUES ('default-ice-can11', E'\\x06', 'dbc: 31|8@0+ (0.78125,-100) [-100|99.21875] "%"', 10, 'CAN11_500', 'shortFuelTrim');
 INSERT INTO pid_configs (template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES ('default-ice-can11', E'\\x11', '31|8@0+ (0.39216,0) [0|100] "%"', 5, 'CAN11_500', 'throttlePosition');
+VALUES ('default-ice-can11', E'\\x11', 'dbc: 31|8@0+ (0.39216,0) [0|100] "%"', 5, 'CAN11_500', 'throttlePosition');
 INSERT INTO pid_configs (template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES ('default-ice-can11', E'\\x07', '31|8@0+ (0.78125,-100) [-100|99.21875] "%"', 60, 'CAN11_500', 'longFuelTrim');
+VALUES ('default-ice-can11', E'\\x07', 'dbc: 31|8@0+ (0.78125,-100) [-100|99.21875] "%"', 60, 'CAN11_500', 'longFuelTrim');
 INSERT INTO pid_configs (template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES ('default-ice-can11', E'\\x0b', '31|8@0+ (1,0) [0|255] "kPa"', 10, 'CAN11_500', 'intakePressure');
+VALUES ('default-ice-can11', E'\\x0b', 'dbc: 31|8@0+ (1,0) [0|255] "kPa"', 10, 'CAN11_500', 'intakePressure');
 INSERT INTO pid_configs (template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES ('default-ice-can11', E'\\x10', '31|16@0+ (0.01,0) [0|655.35] "grams/sec"', 10, 'CAN11_500', 'maf');
+VALUES ('default-ice-can11', E'\\x10', 'dbc: 31|16@0+ (0.01,0) [0|655.35] "grams/sec"', 10, 'CAN11_500', 'maf');
 INSERT INTO pid_configs (template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES ('default-ice-can11', E'\\x33', '31|8@0+ (1,0) [0|255] "kPa"', 60, 'CAN11_500', 'barometricPressure');
+VALUES ('default-ice-can11', E'\\x33', 'dbc: 31|8@0+ (1,0) [0|255] "kPa"', 60, 'CAN11_500', 'barometricPressure');
 INSERT INTO pid_configs (template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES ('default-ice-can11', E'\\x0d', '31|8@0+ (1,0) [0|255] "km/h"', 10, 'CAN11_500', 'vehicleSpeed');
+VALUES ('default-ice-can11', E'\\x0d', 'dbc: 31|8@0+ (1,0) [0|255] "km/h"', 10, 'CAN11_500', 'vehicleSpeed');
 INSERT INTO pid_configs (template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES ('default-ice-can11', E'\\x04', '31|8@0+ (0.39216,0) [0|100] "%"', 10, 'CAN11_500', 'engineLoad');
+VALUES ('default-ice-can11', E'\\x04', 'dbc: 31|8@0+ (0.39216,0) [0|100] "%"', 10, 'CAN11_500', 'engineLoad');
 INSERT INTO pid_configs (template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES ('default-ice-can11', E'\\x46', '31|8@0+ (1,-40) [-40|215] "Celcius"', 60, 'CAN11_500', 'ambientAirTemp');
+VALUES ('default-ice-can11', E'\\x46', 'dbc: 31|8@0+ (1,-40) [-40|215] "Celcius"', 60, 'CAN11_500', 'ambientAirTemp');
 INSERT INTO pid_configs (template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES ('default-ice-can11', E'\\x0f', '31|8@0+ (1,-40) [-40|215] "Celcius"', 10, 'CAN11_500', 'intakeTemp');
+VALUES ('default-ice-can11', E'\\x0f', 'dbc: 31|8@0+ (1,-40) [-40|215] "Celcius"', 10, 'CAN11_500', 'intakeTemp');
 INSERT INTO pid_configs (template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES ('default-ice-can11', E'\\x2f', '31|8@0+ (0.392156862745098,0) [0|100] "%"', 60, 'CAN11_500', 'fuelLevel');
+VALUES ('default-ice-can11', E'\\x2f', 'dbc: 31|8@0+ (0.392156862745098,0) [0|100] "%"', 60, 'CAN11_500', 'fuelLevel');
 INSERT INTO pid_configs (template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES ('default-ice-can11', E'\\x0c', '31|16@0+ (0.25,0) [0|16383.75] "rpm"', 5, 'CAN11_500', 'rpm');
+VALUES ('default-ice-can11', E'\\x0c', 'dbc: 31|16@0+ (0.25,0) [0|16383.75] "rpm"', 5, 'CAN11_500', 'rpm');
 INSERT INTO pid_configs (template_name, pid, formula, interval_seconds, protocol, signal_name)
 VALUES ('default-ice-can11', E'\\x05', 'dbc: 31|8@0+ (1,-40) [-40|215] "Celcius"', 30, 'CAN11_500', 'coolantTemp');
 
 -- 2019+ with odometer
 INSERT INTO pid_configs (template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES ('2019plus-ice-can11', E'\\xa6', '31|32@0+ (0.1,0) [1|429496730] "km"', 60, 'CAN11_500', 'odometer');
+VALUES ('2019plus-ice-can11', E'\\xa6', 'dbc: 31|32@0+ (0.1,0) [1|429496730] "km"', 60, 'CAN11_500', 'odometer');
 
 --- protocol 7 / can 29
 INSERT INTO pid_configs (header, template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x30', '31|8@0+ (1,0) [0|255] "count"', 0, 'CAN29_500', 'warmupsSinceDtcClear');
+VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x30', 'dbc: 31|8@0+ (1,0) [0|255] "count"', 0, 'CAN29_500', 'warmupsSinceDtcClear');
 INSERT INTO pid_configs (header, template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x31', '31|16@0+ (1,0) [0|65535] "km" ', 0, 'CAN29_500', 'distanceSinceDtcClear');
+VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x31', 'dbc: 31|16@0+ (1,0) [0|65535] "km" ', 0, 'CAN29_500', 'distanceSinceDtcClear');
 INSERT INTO pid_configs (header, template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x21', '31|16@0+ (1,0) [0|65535] "km"', 0, 'CAN29_500', 'distanceWMil');
+VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x21', 'dbc: 31|16@0+ (1,0) [0|65535] "km"', 0, 'CAN29_500', 'distanceWMil');
 INSERT INTO pid_configs (header, template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x2c', '31|8@0+ (0.392156862745098,0) [0|100] "%"', 20, 'CAN29_500', 'commandedEgr');
+VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x2c', 'dbc: 31|8@0+ (0.392156862745098,0) [0|100] "%"', 20, 'CAN29_500', 'commandedEgr');
 INSERT INTO pid_configs (header, template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x1f', '31|16@0+ (1,0) [0|65535] "seconds"', 30, 'CAN29_500', 'runTime');
+VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x1f', 'dbc: 31|16@0+ (1,0) [0|65535] "seconds"', 30, 'CAN29_500', 'runTime');
 INSERT INTO pid_configs (header, template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x06', '31|8@0+ (0.78125,-100) [-100|99.21875] "%"', 10, 'CAN29_500', 'shortFuelTrim');
+VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x06', 'dbc: 31|8@0+ (0.78125,-100) [-100|99.21875] "%"', 10, 'CAN29_500', 'shortFuelTrim');
 INSERT INTO pid_configs (header, template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x11', '31|8@0+ (0.39216,0) [0|100] "%"', 5, 'CAN29_500', 'throttlePosition');
+VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x11', 'dbc: 31|8@0+ (0.39216,0) [0|100] "%"', 5, 'CAN29_500', 'throttlePosition');
 INSERT INTO pid_configs (header, template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x07', '31|8@0+ (0.78125,-100) [-100|99.21875] "%"', 60, 'CAN29_500', 'longFuelTrim');
+VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x07', 'dbc: 31|8@0+ (0.78125,-100) [-100|99.21875] "%"', 60, 'CAN29_500', 'longFuelTrim');
 INSERT INTO pid_configs (header, template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x0b', '31|8@0+ (1,0) [0|255] "kPa"', 10, 'CAN29_500', 'intakePressure');
+VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x0b', 'dbc: 31|8@0+ (1,0) [0|255] "kPa"', 10, 'CAN29_500', 'intakePressure');
 INSERT INTO pid_configs (header, template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x10', '31|16@0+ (0.01,0) [0|655.35] "grams/sec"', 10, 'CAN29_500', 'maf');
+VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x10', 'dbc: 31|16@0+ (0.01,0) [0|655.35] "grams/sec"', 10, 'CAN29_500', 'maf');
 INSERT INTO pid_configs (header, template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x33', '31|8@0+ (1,0) [0|255] "kPa"', 60, 'CAN29_500', 'barometricPressure');
+VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x33', 'dbc: 31|8@0+ (1,0) [0|255] "kPa"', 60, 'CAN29_500', 'barometricPressure');
 INSERT INTO pid_configs (header, template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x0d', '31|8@0+ (1,0) [0|255] "km/h"', 10, 'CAN29_500', 'vehicleSpeed');
+VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x0d', 'dbc: 31|8@0+ (1,0) [0|255] "km/h"', 10, 'CAN29_500', 'vehicleSpeed');
 INSERT INTO pid_configs (header, template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x04', '31|8@0+ (0.39216,0) [0|100] "%"', 10, 'CAN29_500', 'engineLoad');
+VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x04', 'dbc: 31|8@0+ (0.39216,0) [0|100] "%"', 10, 'CAN29_500', 'engineLoad');
 INSERT INTO pid_configs (header, template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x46', '31|8@0+ (1,-40) [-40|215] "Celcius"', 60, 'CAN29_500', 'ambientAirTemp');
+VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x46', 'dbc: 31|8@0+ (1,-40) [-40|215] "Celcius"', 60, 'CAN29_500', 'ambientAirTemp');
 INSERT INTO pid_configs (header, template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x0f', '31|8@0+ (1,-40) [-40|215] "Celcius"', 10, 'CAN29_500', 'intakeTemp');
+VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x0f', 'dbc: 31|8@0+ (1,-40) [-40|215] "Celcius"', 10, 'CAN29_500', 'intakeTemp');
 INSERT INTO pid_configs (header, template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x2f', '31|8@0+ (0.392156862745098,0) [0|100] "%"', 60, 'CAN29_500', 'fuelLevel');
+VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x2f', 'dbc: 31|8@0+ (0.392156862745098,0) [0|100] "%"', 60, 'CAN29_500', 'fuelLevel');
 INSERT INTO pid_configs (header, template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x0c', '31|16@0+ (0.25,0) [0|16383.75] "rpm"', 5, 'CAN29_500', 'rpm');
+VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x0c', 'dbc: 31|16@0+ (0.25,0) [0|16383.75] "rpm"', 5, 'CAN29_500', 'rpm');
 INSERT INTO pid_configs (header, template_name, pid, formula, interval_seconds, protocol, signal_name)
 VALUES (E'\\x18DB33F1', 'default-ice-can29', E'\\x05', 'dbc: 31|8@0+ (1,-40) [-40|215] "Celcius"', 30, 'CAN29_500', 'coolantTemp');
 
@@ -97,4 +97,4 @@ INSERT INTO templates (template_name, parent_template_name, template_type, versi
 VALUES ('2019plus-ice-can29', 'default-ice-can29', null, 'v1.0.0', 'CAN29_500', 'ICE');
 
 INSERT INTO pid_configs (header, template_name, pid, formula, interval_seconds, protocol, signal_name)
-VALUES (E'\\x18DB33F1', '2019plus-ice-can29', E'\\xa6', '31|32@0+ (0.1,0) [1|429496730] "km"', 60, 'CAN29_500', 'odometer');
+VALUES (E'\\x18DB33F1', '2019plus-ice-can29', E'\\xa6', 'dbc: 31|32@0+ (0.1,0) [1|429496730] "km"', 60, 'CAN29_500', 'odometer');
