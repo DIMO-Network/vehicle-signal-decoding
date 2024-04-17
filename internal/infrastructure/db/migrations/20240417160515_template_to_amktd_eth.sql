869ff21aaf6a1adc2f9a3de24e0c116dd03c1fbf
Eduardo Rodriguez <eduardo.rodriguezz@outlook.com>
2024-04-17 19:00:09 +0200

    [SM-757] add migration

---
+++ internal/infrastructure/db/migrations/20240417160515_template_to_amktd_eth.sql
@@ -0,0 +1,29 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+SET search_path = vehicle_signal_decoding_api, public;
+DROP TABLE IF EXISTS serial_to_template_overrides CASCADE;
+CREATE TABLE IF NOT EXISTS aftermarket_device_to_template_overrides (
+  aftermarket_device_ethereum_address bytea NOT NULL,
+  template_name TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+  CONSTRAINT serial_to_template_overrides_pkey PRIMARY KEY (aftermarket_device_ethereum_address, template_name)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+SET search_path = vehicle_signal_decoding_api, public;
+CREATE TABLE IF NOT EXISTS serial_to_template_overrides (
+    serial TEXT NOT NULL,
+    template_name TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT serial_to_template_overrides_pkey PRIMARY KEY (serial, template_name)
+);
+DROP TABLE IF EXISTS aftermarket_device_to_template_overrides CASCADE;
+-- +goose StatementEnd
