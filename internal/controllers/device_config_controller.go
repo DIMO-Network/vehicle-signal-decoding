d13b1dcc92234fdd4a53049d23e0ea5a1a665d46
dmytrozakharenko <amidum@gmail.com>
2024-07-09 13:53:35 -0400

    - typo

--- internal/controllers/device_config_controller.go
+++ internal/controllers/device_config_controller.go
@@ -490,7 +490,7 @@ func (d *DeviceConfigController) PatchConfigStatusByEthAddr(c *fiber.Ctx) error 
 }
 
 // PatchHwConfigStatusByEthAddr godoc
-// @Description  Set what template and/or firmware was applied. None of the properties are required. Will not be set if not passed in.  Endpoint is meant only for hardware devices self-reporting their template update.
+// @Description  Set what template and/or firmware was applied. None of the properties are required. Will not be set if not passed in. Endpoint is meant only for hardware devices self-reporting their template update.
 // @Tags         device-config
 // @Produce      json
 // @Success      200 "Successfully updated"
